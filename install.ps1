18967fb33744399a3aaa9ade6800506362228f71
thomasbonsirven <thomasbonsirven@gmail.com>
2023-08-13 12:54:15 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -8,8 +8,27 @@ if($hyperv.State -eq "Enabled") {
 }
 
 # After install hyper-v, download pre-request
-## Windows 10 super-light
-### Package FR
-### Steam cla
-### Suchine Host Gaming + Custom config ( Port + Game - Desktop access )
+## Création d'une vm
+### Création d'un adapateur reseau externe 
+#### Vérification de l'existance du nom
+$tt = (Get-NetAdapter -physical | where status -eq 'up').name
+foreach ($name in $tt) { if ($name -eq "Ethernet") { New-VMSwitch -Name "External"  -NetAdapterName "Ethernet" }else{
+Write-Host "non $name"
+(Get-NetAdapter -physical | where status -eq 'up').name
+
+
+    }
+}
+
+
+
+
+
+New-VM -Name "VM-TEST" -Path "C:\VM" -MemoryStartupBytes 10GB -NewVHDPath "C:\VM\VM-TEST\VM-TEST-C.vhdx" -NewVHDSizeBytes 120GB -Generation 2 -Switch "LAB_RDR" -BootDevice NetworkAdapter
+Set-VM -Name "VM-TEST" -ProcessorCount 2 -CheckpointType Disabled
+
+### Windows 10 super-light
+#### Package FR
+#### Steam cla
+#### Suchine Host Gaming + Custom config ( Port + Game - Desktop access )
 
