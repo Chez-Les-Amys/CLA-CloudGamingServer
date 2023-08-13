283e378cb4e6b3c857d16b2261b24cd7440fc2d1
thomasbonsirven <thomasbonsirven@gmail.com>
2023-08-13 17:58:48 +0200

    Update install.ps1
    Prepare VM Variable

--- install.ps1
+++ install.ps1
@@ -1,3 +1,11 @@
+# Déclaration des variables
+$cardName = ""
+$vnName = ""
+$vmRAM = 0
+$vmProc = 0
+$vmSpaceDisk = 0
+
+
 # Check and install if necessary Hyper-v
 $hyperv = Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online
 # Check if Hyper-V is enabled
@@ -16,23 +24,63 @@ cls
 $cardName = Read-Host "Veuillez inscrir le nom de votre carte reseau. Par defaut 'Ethernet' "
 if ($cardName -eq ""){ $cardName = "Ethernet"}
 $tt = (Get-NetAdapter -physical | where status -eq 'up').name
-foreach ($name in $tt) { if ($name -eq "$cardName") { 
-
-New-VMSwitch -Name "External"  -NetAdapterName "$cardName" }else{
-
-Write-Host "Le reseau '$cardName' n'existe pas, merci de verifié le nom ci-apres et recommencer la procédure."
-(Get-NetAdapter -physical | where status -eq 'up').name
-
-
+    foreach ($name in $tt) 
+    { 
+        if ($name -eq "$cardName") { 
+    
+            New-VMSwitch -Name "External"  -NetAdapterName "$cardName" }
+        else {
+    
+            Write-Host "Le reseau '$cardName' n'existe pas, merci de verifié le nom ci-apres et recommencer la procédure."
+            (Get-NetAdapter -physical | where status -eq 'up').name
+        }
     }
+cls
+# Préparation de la création de la VM
+## Demande de la quantité RAM
+cls
+for ($i=0 ;$i -le 0){
+    if ($vmName  -eq ""){$vmName = Read-Host "Veuillez choisir un nom pour la VM"}else {
+    $i++ } 
 }
+Do { $vmRAM = Read-host "Choix de la quantité de la RAM en GB ( de 4 à 32GB ) "} 
+while ((4..32) -notcontains $vmRAM)
+
+## Demande de la quantité CPU
+cls
+$vmProcget = (Get-WmiObject –class Win32_processor).NumberOfLogicalProcessors
+$vmProcMax = $vmProcget-2
+Do { $vmProc = Read-host "Choix de la quantité du processeur de 2 à $vmProcMax "} 
+while ((4.."$vmProcMax") -notcontains $vmProc)
+
+## Demande de la quantité HDD
+cls
+write-host "Mes lettre de mes disques " 
+$driveLetter = (Get-PSDrive).Name -match '^[a-z]$'
+$driveLetter
+$getDiskLetterByUser = Read-host "Merci de choisir votre lecteur pour le disk de la VM" 
+
+ foreach ($letter in $driveLetter) 
+    { if ($getDiskLetterByUser -eq "$letter") { 
+        $getDiskFreeSpace = (Get-PSDrive $getDiskLetter).Free
+        $getDiskFreeSpaceinGB = [math]::Floor($getDiskFreeSpace /1GB)
+       
+        Do { $vmSpaceDisk = Read-host "Choix de la quantité du HDD en GB ( de 25 à ""$getDiskFreeSpaceinGB""GB) "} 
+        while ((25..$getDiskFreeSpaceinGB) -notcontains $vmSpaceDisk)
+
+
+
+      }
+    }
 
 
 
 
-
-New-VM -Name "VM-TEST" -Path "C:\VM" -MemoryStartupBytes 10GB -NewVHDPath "C:\VM\VM-TEST\VM-TEST-C.vhdx" -NewVHDSizeBytes 120GB -Generation 2 -Switch "$cardName" -BootDevice NetworkAdapter
+## Création de la VM
+cls
+New-VM -Name "$vmName" -Path "$getDiskLetterByUser:\VM" -MemoryStartupBytes "$vmRAM" -NewVHDPath "$getDiskLetterByUser:\VM\VM-TEST\$vmName.vhdx" -NewVHDSizeBytes "$vmSpaceDisk"GB -Generation 2 -Switch "External" -BootDevice NetworkAdapter
 Set-VM -Name "VM-TEST" -ProcessorCount 2 -CheckpointType Disabled
+
 
 ### Windows 10 super-light
 #### Package FR
