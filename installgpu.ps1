1534d8e2447db836f5f4ee1f85351c728ef0e18b
thomasbonsirven <thomasbonsirven@gmail.com>
2023-08-14 16:51:17 +0200

    Create installgpu.ps1

---
+++ installgpu.ps1
@@ -0,0 +1,8 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+
+Copy-Item -Path $PSScriptRoot\System32  -Destination C:\Windows\ -Recurse
